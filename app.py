ff605e6efc11c086f2bb9c689508b01e86fb8bd2
yashksaini-coder <ys3853428@gmail.com>
2024-11-15 19:50:29 +0530

    refactor: 💡 Add predict function + Misc stuff
    ✅ Closes: #5

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask, render_template
+import joblib
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
+
+scaler = StandardScaler()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/predict')
-def predict_page():
-    return render_template('predict.html')
 
 @app.route('/understand_data')
 def understand_data():
     return render_template('understand_data.html')
 
 @app.route('/predict', methods=['POST'])
-def predict():
-    data = request.form
-    hours_studied = float(data['hours_studied'])
-    previous_scores = float(data['previous_scores'])
-    extracurricular_activities = 1.0 if data['extracurricular_activities'] == 'yes' else 0.0
-    sleep_hours = float(data['sleep_hours'])
-    sample_question_papers_practiced = float(data['sample_question_papers_practiced'])
-
-    # Prediction formula
-    y = (2.85 * hours_studied + 
-         1.02 * previous_scores + 
-         0.61 * extracurricular_activities + 
-         0.48 * sleep_hours + 
-         0.19 * sample_question_papers_practiced + 
-         (-33.92))
-
-    return jsonify({'prediction': y})
+def predict_crop(N, P, K, temperature, humidity, ph, rainfall):
+    # Load the SeedySense model
+    seedy_model = joblib.load("../model/SeedySense.pkl")
+    # Create a dataframe with the input values
+    input_data = pd.DataFrame({
+        'N': [N],
+        'P': [P],
+        'K': [K],
+        'temperature': [temperature],
+        'humidity': [humidity],
+        'ph': [ph],
+        'rainfall': [rainfall]
+    })
+    # Scale the input data
+    input_data_scaled = scaler.transform(input_data)
+    prediction = seedy_model.predict(input_data_scaled)
+    return prediction[0]
 
 if __name__ == '__main__':
     app.run(debug=True)
